72679ab827ccb539979b378b58377b3cce55b19a
toibitramcam <95275301+cdini@users.noreply.github.com>
2022-01-21 00:21:07 +0000

    fix bug

--- Program.cs
+++ Program.cs
@@ -16,29 +16,27 @@ class Program {
     Console.WriteLine("\nHo ten: "+sinhvien01.hoten+"\nMa sinh vien: "+sinhvien01.masv+"\nDia chi:"+sinhvien01.diachi);
 
     SV sinhvien02;
-    sinhvien02.hoten = "Nguyen Van B";
-    sinhvien02.masv = "20000741";
-    sinhvien02.diachi = "127/75";
+    sinhvien02.hoten = "Nguyen Van Teo";
+    sinhvien02.masv = "20003369";
+    sinhvien02.diachi = "C3/11M";
     Console.WriteLine("\nHo ten: "+sinhvien02.hoten+"\nMa sinh vien: "+sinhvien02.masv+"\nDia chi:"+sinhvien02.diachi);
 
-   
     SV sinhvien03;
-    sinhvien03.hoten = "Nguyen Van C";
+    sinhvien03.hoten = "Bui Tien Dung";
     sinhvien03.masv = "20000769";
     sinhvien03.diachi = "C3/11M";
     Console.WriteLine("\nHo ten: "+sinhvien03.hoten+"\nMa sinh vien: "+sinhvien03.masv+"\nDia chi:"+sinhvien03.diachi);
 
-
     SV sinhvien04;
-    sinhvien04.masv = "20000769";
-    sinhvien04.diachi = "C3/11M";
+    sinhvien04.hoten = "Nguyen Tuan Anh";
+    sinhvien04.masv = "20000469";
+    sinhvien04.diachi = "C5/11M";
     Console.WriteLine("\nHo ten: "+sinhvien04.hoten+"\nMa sinh vien: "+sinhvien04.masv+"\nDia chi:"+sinhvien04.diachi);
 
-    
     SV sinhvien05;
-    sinhvien05.hoten = "Nguyen Van E";
-    sinhvien05.masv = "20000769";
-    sinhvien05.diachi = "C3/11M";
+    sinhvien05.hoten = "Nguyen Van Quyet";
+    sinhvien05.masv = "20000169";
+    sinhvien05.diachi = "127/85";
     Console.WriteLine("\nHo ten: "+sinhvien05.hoten+"\nMa sinh vien: "+sinhvien05.masv+"\nDia chi:"+sinhvien05.diachi);
 
     Console.ReadLine();
